ec617f40fe8f53aa65d4305c321b679472b88e38
Joseph Clark <joseph@thellama.us>
2018-03-19 15:32:07 -0400

    Changes for python 3

--- battleship.py
+++ battleship.py
@@ -3,7 +3,7 @@ import sys
 
 def print_board(board):
     for row in board:
-        print " ".join(row)
+        print(" ".join(row))
 
 def random_row(board):
     return randint(0, len(board) - 1)
@@ -12,49 +12,49 @@ def random_col(board):
     return randint(0, len(board[0]) - 1)
 
 def get_guess():
-    guess_row = int(raw_input("Guess Row: "))
-    guess_col = int(raw_input("Guess Col: "))
+    guess_row = int(input("Guess Row: "))
+    guess_col = int(input("Guess Col: "))
     return guess_row, guess_col
 
 def get_board_size():
-    guess_row = int(raw_input("Guess Row: "))
-    guess_col = int(raw_input("Guess Col: "))
+    guess_row = int(input("Guess Row: "))
+    guess_col = int(input("Guess Col: "))
     return guess_row, guess_col
 
 def check_hit(board, row_ship, row_guess, col_ship, col_guess, size_board):
     if row_guess == row_ship and col_guess == col_ship:
-        print "\nCongratulations! You sank my battleship!\n"
+        print("\nCongratulations! You sank my battleship!\n")
         board[row_guess][col_guess] = "X"
         return True
     else:
         if row_guess not in range(size_board) or \
             col_guess not in range(size_board):
-            print "\nOops, that's not even in the ocean."
+            print("\nOops, that's not even in the ocean.")
         elif board[row_guess][col_guess] == "X":
-            print "\nYou guessed that one already."
+            print("\nYou guessed that one already.")
         else:
-            print "\nYou missed my battleship!"
+            print("\nYou missed my battleship!")
             board[row_guess][col_guess] = "X"
-        print "Please guess again\n"
+        print("Please guess again\n")
     print_board(board)
-    print
+    print("\n")
     return board
 
 def would_continue():
-    to_continue = raw_input("Would you like to continue? respond Y or N: ")
+    to_continue = input("Would you like to continue? respond Y or N: ")
     if to_continue == "N" or to_continue == "n":
         sys.exit()
 
 def main():
     board = []
-    board_size = int(raw_input("\nSize of board please: "))
+    board_size = int(input("\nSize of board please: "))
 
     for x in range(0, board_size):
         board.append(["O"] * board_size)
 
     ship_row = random_row(board)
     ship_col = random_col(board)
-    print ship_row, ship_col
+    print(ship_row, ship_col)
 
     guess_row, guess_col = get_guess()
 
@@ -62,7 +62,7 @@ def main():
 
     while guess_hit != True:
         would_continue()
-        print "just a reminder, the ship is at coord: ", ship_row, ship_col
+        print("just a reminder, the ship is at coord: ", ship_row, ship_col)
         guess_row, guess_col = get_guess()
         guess_hit = check_hit(board, ship_row, guess_row, ship_col, guess_col, board_size)
 
